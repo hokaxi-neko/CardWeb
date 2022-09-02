9b0810b976c5907f127a7d2862387ece4e45951d
sean <iolupio@gmail.com>
2022-09-02 18:43:01 +0800

    還原main

--- CardWebApp/CardWebApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CardWebApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
